eb4703553e4ffdce7823d8b080a596cdbfcf9067
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-04-28 11:25:28 +0900

    mainPage 세팅

--- routers/main.js
+++ routers/main.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const {} = require("../controllers/main");
+
+//메인페이지 조회
+router.get("/readMain");
+
+//메인페이지 검색
+router.get("/searchMain");
+
 module.exports = router;
