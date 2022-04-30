6c67ddda03f8622bf597492bd75640eee3b71970
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-04-30 10:20:41 +0900

    제발되라

--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../schemas/user')
-const jwt = require('jsonwebtoken')
-const authMiddleware = require('../middleware/authMiddleWare')
-
-
-
 
 module.exports = router;
