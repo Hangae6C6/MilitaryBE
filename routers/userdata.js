d5dce36ef8bc9aae30adcfcf377c20083525de91
jason taekyun Kim <saurus234@gmail.com>
2022-05-02 13:49:15 +0900

    add router userOptionalData

--- routers/userdata.js
+++ routers/userdata.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { userOptioalData } = require("../controllers/userData");
+const authMiddleware = require("../middleware/authMiddleWare");
+require("dotenv").config();
+
+router.post("/modal/userData", authMiddleware, userOptioalData);
 
 module.exports = router;
