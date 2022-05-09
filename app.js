e23053eaddc38e2f8a5d67e7ef7b7db17629c2d1
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-09 19:25:05 +0900

    socket test

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ app.get("/", async (req, res) => {
    });
 
 //서버 열기..
-http.listen(port, ()=> winston.info(`${port} 포트로 서버가 켜졌어요!`))
+// http.listen(port, ()=> winston.info(`${port} 포트로 서버가 켜졌어요!`))
 server.listen(port, ()=> winston.info(`${port} 포트로 서버가 켜졌어요!`))
 // app.listen(4000, ()=> winston.info('4000 포트로 서버가 켜졌어요!'))
 
