b9c815d00f14ac1600fe1d111aa74b76e5ff6bc5
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-09 17:07:34 +0900

    socket test 35

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
       socket.join(data);
       console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      socket.to(data).emit("join-msg", `${socket["userName"]}님께서 입장하셨습니다.`)
     });
   
     socket.on("send_message", (data) => {
