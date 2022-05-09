12b6620eebd233dc8656e1baa402698003c39fb5
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-09 13:48:47 +0900

    소켓테스트 16

--- app.js
+++ app.js
@@ -58,13 +58,9 @@ io.on("connection", (socket) => {
         console.log(data)
     })
 
-    socket.on("leave-room", (roomName, done)=> {
-        socket.leave(roomName)
-        done();
-        const rooms = getUserRooms();
-        if (!rooms.includes(roomName)) {
-            io.emit("remove-room", roomName)
-        }
+    socket.on("leave-room", (room)=> {
+        socket.leave(room)
+        io.to(room).emit(`${socket.io}님이 퇴장하셨습니다.`)
     })
 
     socket.on("disconnect", ()=> {
