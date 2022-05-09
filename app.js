54059a6f37fde850adcd174f49d1c092de133c22
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-09 16:47:31 +0900

    socket test 31

--- app.js
+++ app.js
@@ -45,70 +45,22 @@ const io = new Server(server, {
   },
 });
 
-const getSids = () => {
-    const ids = [];
-    const { sids, rooms } = io.of("/").adapter;
-    
-    rooms.forEach((_, key) => {
-      if (sids.get(key)) {
-        ids.push(key);
-      }
-    });
-    return ids;
-  };
-  const getUserRooms = () => {
-    const userRooms = [];
-    const { sids, rooms } = io.of("/").adapter;
-    rooms.forEach((_, key) => {
-      if (sids.get(key) === undefined) {
-        userRooms.push(key);
-      }
-    });
-    return userRooms;
-  };
-  const updateRoomList = () => {
-    const ids = getSids();
-    const userRooms = getUserRooms();
-    ids.forEach(id => io.to(id).emit("updateRooms", userRooms));
-  };
-
-
-  io.on("connection", socket => {
-    socket.on("leave-room", (roomName, done) => {
-      socket.leave(roomName);
-      done();
-      const rooms = getUserRooms();
-      if (!rooms.includes(roomName)) {
-        io.emit("remove-room", roomName);
-      }
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
   
-    socket.on("message", (msg, roomName, done) => {
-      done();
-      socket.broadcast.to(roomName).emit("message", msg);
-    });
-    socket.on("join-room", (roomName, done) => {
-      socket.join(roomName);
-      done();
-      socket
-        .to(roomName)
-        .emit("join-msg", `${socket["nickname"]}님께서 입장하셨습니다. !!!`);
-    });
-    socket.on("login", () => {
-      io.to(socket.id).emit("updateRooms", getUserRooms());
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
     });
   
-    socket.on("create-room", (roomName, done) => {
-      console.log("create-room", roomName);
-      socket.join(roomName);
-      done();
-      updateRoomList();
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
     });
-    socket.on("nickname", (nickname, done) => {
-      socket["nickname"] = nickname;
-      done();
-    });
-});
+  });
 
 //라우터 불러오기
 const userRouter = require("./routers/user");
