df080ce18250848101fb55bf46ba98bbb92df68e
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-10 10:49:20 +0900

    Update main.js

--- routers/main.js
+++ routers/main.js
@@ -12,12 +12,14 @@ const {
 } = require("../controllers/main");
 const authMiddleware = require("../middleware/authMiddleWare");
 
-router.get("/main", mainPage);
+router.get("/main", mainPage); //회원 + 비회원 둘다 가능
 
 //회원님 참여 챌린지
-router.get("/main/challenge", authMiddleware, userChallenge);
+router.get("/main/challenge", authMiddleware, userChallenge); //회원만 가능
 
-router.post("/main/preTest", authMiddleware, preTest);
+router.post("/main/preTest", authMiddleware, preTest); // 회원만 가능
+
+router.post("/main/preTest", preTest); // 비회원도 가능 
 
 router.get("/search", search);
 
