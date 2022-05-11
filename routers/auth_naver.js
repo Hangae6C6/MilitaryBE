d50a2412c7be57b03984f19caba052dbde3df24c
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-11 17:19:18 +0900

    naver login upload

--- routers/auth_naver.js
+++ routers/auth_naver.js
@@ -6,6 +6,9 @@ dotenv.config()
 
 const router = express.Router()
 
+const jwtSecretKey = process.env.JWT_SECRET;
+const jwtExpiresInDays = "2d";
+
 //네이버 로그인 하기
 router.get('/naver',passport.authenticate('naver'))
 
@@ -20,7 +23,7 @@ router.get('/oauth', passport.authenticate('naver',{
         console.log("여긴 ok 잘됩니당")
         //쿠키로 토큰을 발급한 후 리다이렉트
         //사용자 인증이 끝나면 돌아갈 URL주소
-        res.status(200).redirect("??")
+        res.status(200).redirect("http://3.34.98.31/api/naver?token="+token)
     }catch (error) {
         console.log(error)
         console.log("auth_naver.js 콜백url -> 여기서 에러발생함")
@@ -28,4 +31,7 @@ router.get('/oauth', passport.authenticate('naver',{
     }
 }
 )
+function createJwtToken(id) {
+    return jwt.sign({ id }, jwtSecretKey, { expiresIn: jwtExpiresInDays });
+}
 module.exports = router
