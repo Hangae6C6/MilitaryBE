214a52ad42874f8039a1bd37842f547c33458cc7
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-11 17:39:50 +0900

    upload

--- passport/NaverStrategy.js
+++ passport/NaverStrategy.js
@@ -22,17 +22,28 @@ passport.use(
             clientSecret:process.env.NAVER_CLIENT_SECRET,
             callbackURL:'http://localhost:3000/api/auth/naver/callback', // 애플리케이션을 등록할 떄 입력했던 callbackURL 을 입력해준다.
         },
-        function(accessToken, refreshToken, profile, done){
-                const _profile = profile._json;
-
-                loginByThirdparty({
-                   socialtype:'naver',
-                   socialId :profile.id,
-                   nickname :profile._json.nickname,
-                }, done)
-                
+        async (accessToken, refreshToken, profile, cb) => {
+            try {
+              const user = await User.findOne({ socialId: profile.id });
+      
+              //동일한 이메일을 가졌을 때는 이미 가입중인 사용자라면 바로 로그인하도록 아니라면 신규 사용자 생성
+              if (user) {
+                user.socialId = profile.id;
+                user.save();
+                return cb(null, user);
+              } else {
+                const newUser = await User.create({
+                  socialtype: "naver",
+                  socialId: profile.id,
+                  nickname: profile._json.nickname,
+                });
+                return cb(null, newUser);
+              }
+            } catch (error) {
+              return cb(error);
             }
-    )
+          }
+        )
 )
 
 module.exports = passport
