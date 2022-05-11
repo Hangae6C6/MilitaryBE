e6200019d92014c55d480bee0d3fb89a2772b398
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-11 17:28:18 +0900

    naver login

--- routers/auth_naver.js
+++ routers/auth_naver.js
@@ -16,20 +16,20 @@ router.get('/naver',passport.authenticate('naver'))
 router.get('/oauth', passport.authenticate('naver',{
     session:false,
     failureRedirect: "/",
-}),
-(req,res)=>{
-    try {
-        const token = createJwtToken(req.user._id);
-        console.log("여긴 ok 잘됩니당")
-        //쿠키로 토큰을 발급한 후 리다이렉트
-        //사용자 인증이 끝나면 돌아갈 URL주소
-        res.status(200).redirect("http://3.34.98.31/api/naver?token="+token)
-    }catch (error) {
-        console.log(error)
-        console.log("auth_naver.js 콜백url -> 여기서 에러발생함")
-        res.status(400).json({result:false,msg:"콜백 실패..."})
-    }
-}
+// }),
+// (req,res)=>{
+//     try {
+//         const token = createJwtToken(req.user._id);
+//         console.log("여긴 ok 잘됩니당")
+//         //쿠키로 토큰을 발급한 후 리다이렉트
+//         //사용자 인증이 끝나면 돌아갈 URL주소
+//         res.status(200).redirect("http://3.34.98.31/api/naver?token="+token)
+//     }catch (error) {
+//         console.log(error)
+//         console.log("auth_naver.js 콜백url -> 여기서 에러발생함")
+//         res.status(400).json({result:false,msg:"콜백 실패..."})
+//     }
+})
 )
 function createJwtToken(id) {
     return jwt.sign({ id }, jwtSecretKey, { expiresIn: jwtExpiresInDays });
