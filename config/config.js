68d4f1f0b2e16606bc97fa7a2c8a412c55bc4b95
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-05-19 16:03:11 +0900

    추가

--- config/config.js
+++ config/config.js
@@ -1,21 +1,23 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
     "username": "admin",
-    "password": "hanghae99",
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "soldierChallengers_Node",
     "host": "soldierchallengers.ct6f9issjscy.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
     "username": "admin",
-    "password": "hanghae99",
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "soldierChallengers_Node",
     "host": "soldierchallengers.ct6f9issjscy.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
     "username": "admin",
-    "password": "hanghae99",
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "soldierChallengers_Node",
     "host": "soldierchallengers.ct6f9issjscy.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
