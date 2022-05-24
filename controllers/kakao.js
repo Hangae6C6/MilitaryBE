8195c6a396b5de99dfe599dfc99c459c02a74b27
eorb1320 <eorb910623@gmail.com>
2022-05-24 17:47:57 +0900

    kkaka
    kaka

--- controllers/kakao.js
+++ controllers/kakao.js
@@ -67,8 +67,9 @@ const kakaoRegister = async (req,res) => {
         const loginUser = await User.findOne({where: { userId: userId }});
         const token = jwt.sign({ userId : loginUser[0].userId }, `${process.env.KEY}`); // 이부분 바꿨는데 userId : loginUser.userId --> ? 
 
-        // redi -> main -> ? 
-    
+        console.log("12321321",token,loginUser);
+        // console.log(loginUser)
+
         res.status(200).json({
             token,
             userId,
